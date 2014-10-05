b12b28490406c8c11d10a0d0f036119e8b675828
Thomas Boyt <me@thomasboyt.com>
2014-10-05 02:30:03 -0400

    goddammit vim

--- chrome-extension/content-script.js
+++ chrome-extension/content-script.js
@@ -1,17 +1,15 @@
-console.log('hello i am content script');
-
 window.addEventListener('message', function(event) {
-	// Only accept messages from same frame
-	if (event.source !== window) {
-		return;
-	}
+  // Only accept messages from same frame
+  if (event.source !== window) {
+    return;
+  }
 
-	var message = event.data;
+  var message = event.data;
 
-	// Only accept messages of correct format (our messages)
-	if (typeof message !== 'object' || message === null || message.target !== 'page') {
-		return;
-	}
+  // Only accept messages of correct format (our messages)
+  if (typeof message !== 'object' || message === null || message.target !== 'page') {
+    return;
+  }
 
-	chrome.runtime.sendMessage(message);
+  chrome.runtime.sendMessage(message);
 });
