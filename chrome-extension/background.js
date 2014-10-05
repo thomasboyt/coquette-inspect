ed730c39723a3a08b9dd84b198143760b5e2e8d3
Thomas Boyt <me@thomasboyt.com>
2014-10-05 00:37:35 -0400

    what >:I

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -3,27 +3,27 @@ var connections = {};
 chrome.runtime.onConnect.addListener(function(port) {
 
   // Listen to messages sent from the DevTools page
-	port.onMessage.addListener(function(request) {
-		console.log('incoming message from dev tools page');
+  port.onMessage.addListener(function(request) {
+    console.log('incoming message from dev tools page');
 
-		// Register initial connection
+    // Register initial connection
     if (request.name == 'init') {
       connections[request.tabId] = port;
 
-			port.onDisconnect.addListener(function() {
-				delete connections[request.tabId];
-			});
+      port.onDisconnect.addListener(function() {
+        delete connections[request.tabId];
+      });
 
-			return;
+      return;
     }
-	});
+  });
 
 });
 
 // Receive message from content script and relay to the devTools page for the
 // current tab
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-	console.log('incoming message from injected script');
+  console.log('incoming message from injected script');
 
   // Messages from content scripts should have sender.tab set
   if (sender.tab) {
