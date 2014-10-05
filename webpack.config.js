c5942431bc08e3fd4e169319e61d84bd8465ae87
Thomas Boyt <me@thomasboyt.com>
2014-10-05 15:35:02 -0400

    Fix agent.bundle.js missing Webpack boilerplate

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,7 @@ module.exports = {
   },
 
   plugins: [
-    new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.bundle.js')
+    new webpack.optimize.CommonsChunkPlugin('vendor.bundle.js', ['ui', 'vendor'])
   ],
 
   module: {
