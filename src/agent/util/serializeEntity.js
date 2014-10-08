5d4007c1929ea10516b541c48718da346f24cbca
Thomas Boyt <thomas@ledgerx.com>
2014-10-08 13:35:22 -0400

    Serialize entity properties

--- src/agent/util/serializeEntity.js
+++ src/agent/util/serializeEntity.js
@@ -3,22 +3,71 @@
  * Today is not that day.
  */
 
-var serializeEntity = function(entity) {
-  var clone = {};
+var cloneValue = function(val, seen) {
 
-  var val;
-  for (var prop in entity) {
-    val = entity[prop];
-
-    if (typeof val === 'function' || (typeof val === 'object' && val !== null)) {
-      // someday, something nicer
-      // until then, [object Array]
-      clone[prop] = {}.toString(val);
-    } else {
-      // at this point it's just primitives, so copy that value~
-      clone[prop] = val;
-    }
+  /* Functions */
+  if (typeof val === 'function') {
+    return '[object Function]';
   }
+
+  /* Arrays */
+  if (Array.isArray(val)) {
+    if (seen.has(val)) {
+      return '[[Circular reference]]';
+    }
+
+    seen.set(val, true);
+    return dumbArrayClone(val, seen);
+  }
+
+  /* Objects */
+  if (typeof val === 'object' && val !== null) {
+    // TODO: this is a hack to not serialize crazy & non-transportable things like DOM Nodes,
+    // needs to be way more fine-grained
+    if (val.toString() === '[object Object]') {
+      // don't serialize the Coquette object, which is often stored on entities
+      if (val === window.__coquette__) {
+        return '[[Coquette namespace]]';
+      }
+      if (seen.has(val)) {
+        return '[[Circular reference]]';
+      }
+
+      seen.set(val, true);
+      return dumbObjClone(val, seen);
+    }
+
+    // e.g. [object Foo], hopefully?
+    return val.toString();
+  }
+
+  /* Primitives */
+  return val;
+};
+
+var dumbArrayClone = function(arr, seen) {
+  seen = seen || new WeakMap();
+
+  var clone = arr.map(function(val) {
+    return cloneValue(val, seen);
+  });
+
+  return clone;
+};
+
+var dumbObjClone = function(obj, seen) {
+  var clone = {};
+  seen = seen || new WeakMap();
+
+  for (var key in obj) {
+    clone[key] = cloneValue(obj[key], seen);
+  }
+
+  return clone;
+};
+
+var serializeEntity = function(entity) {
+  var clone = dumbObjClone(entity);
 
   clone.displayName = entity.displayName || entity.constructor.name;
 
--- src/ui/components/EntityList.js
+++ src/ui/components/EntityList.js
@@ -21,12 +21,40 @@ var EntityList = React.createClass({
     this.getFlux().actions.entities.subscribeToEntity(id);
   },
 
+  renderEntityProperties: function(entity) {
+    var items = Object.keys(entity).map((prop) => {
+      var val = entity[prop];
+
+      if (typeof val === 'object' && val !== null) {
+        return (
+          <li key={prop}>
+            <code>{prop}</code>: <code>{this.renderEntityProperties(val)}</code>
+          </li>
+        );
+      }
+
+      return (
+        <li key={prop}>
+          <code>{prop}</code>: <code>{val}</code>
+        </li>
+      );
+    });
+
+    return (
+      <ul>
+        {items}
+      </ul>
+    );
+  },
+
   render: function() {
+
     var items = this.state.entities.map((entity) => (
       <li key={entity.__inspect_uuid__}>
         <a onClick={() => this.handleOpenEntity(entity.__inspect_uuid__)}>
           {entity.displayName || 'unknown entity'}
         </a>
+        {this.renderEntityProperties(entity)}
       </li>
     ));
 
